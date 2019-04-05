9c8fd18796118ce2ecb04b22d60f640a572d2a9d
Mark Lalor <markwlalor@gmail.com>
2019-04-05 16:16:34 -0400

    Add the Python "requests" library to auth1api requirements.

--- auth1api/setup.py
+++ auth1api/setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     description="A library to make requests to your Auth1 instance",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=['requests'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
