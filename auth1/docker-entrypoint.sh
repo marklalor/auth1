69e438badb7bee15c0216d72e460f2915bee55dc
huangy6 <huangy6@yahoo.com>
2019-04-05 23:49:28 -0400

    Docker set up not yet working but has to be pushed now

---
+++ auth1/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+java -jar /app.jar \
+-Djava.security.egd=file:/dev/./urandom \
+--server.port=$PORT \
+--config.databaseConfigurationPath=/db.cnf \
+--config.auth1ConfigurationPath=/app.cnf
